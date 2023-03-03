b5f09de68cb81a53a2ac9abb82293521a0772b62
leroymilo <leroyhaveamailbox@gmail.com>
2023-03-03 09:32:03 +0100

    updated streamlit requirements, made command prototype

--- command/dashboard.py
+++ command/dashboard.py
@@ -1,15 +1,25 @@
 import streamlit as st
 import psycopg2
 
-HOST : str = "255.255.211.1"
-PORT : str = "5000"
-USER : str = "user"
-PWD : str = "password"
-DATABASE : str = "DB_PCO"
+# HOST : str = "255.255.211.1"
+# PORT : str = "5000"
+# USER : str = "user"
+# PWD : str = "password"
+# DATABASE : str = "DB_PCO"
 
-con = psycopg2.connect(host=HOST, port=PORT, user=USER, password=PWD)
-cur = con.cursor()
+# con = psycopg2.connect(host=HOST, port=PORT, user=USER, password=PWD)
+# cur = con.cursor()
 
 if __name__ == "__main__" :
-    container = st.container()
-    
+    on, off = st.container().columns(2)
+    on = on.button("ON", use_container_width=True)
+    off = off.button("OFF", use_container_width=True)
+
+    rooms = []
+    for i in range(4) :
+        room = st.container()
+        rooms.append(room)
+        room.markdown(f"# Salle {i+1}")
+        room.checkbox("Détecteur de présence", key=200+i)
+        vars = room.checkbox("Variateur de luminosité", key=300+i)
+        room.slider("Luminosité", 0, 100, 50, 1, key=400+i, disabled=(not vars))

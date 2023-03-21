11ff3c8a15fb552574ec4502f3b1ece220c9740d
Leroy <leroyhaveamailbox@gmail.com>
2023-03-21 21:22:13 +0100

    updated requirements

--- command/dashboard.py
+++ command/dashboard.py
@@ -1,15 +1,18 @@
 import streamlit as st
 import psycopg2
 
-HOST : str = "255.255.211.1"
-PORT : str = "5000"
-USER : str = "user"
-PWD : str = "password"
-DATABASE : str = "DB_PCO"
+# HOST : str = "255.255.211.1"
+# PORT : str = "5000"
+# USER : str = "Admin"
+# PWD : str = "Lasagnes42"
+# DATABASE : str = "DB_PCO"
 
-con = psycopg2.connect(host=HOST, port=PORT, user=USER, password=PWD)
-cur = con.cursor()
+# con = psycopg2.connect(host=HOST, port=PORT, user=USER, password=PWD)
+# cur = con.cursor()
 
 if __name__ == "__main__" :
-    container = st.container()
+    on_off = st.container()
+    on, off = on_off.columns(2)
+    st.button("ON", use_container_width=True)
+    st.button("OFF", use_container_width=True)
     

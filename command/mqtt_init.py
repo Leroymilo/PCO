c508f766db2f79941720c246cf48a8c91f7b8a38
leroymilo <leroyhaveamailbox@gmail.com>
2023-03-24 10:42:41 +0100

    added auto mqtt reconnect

--- command/mqtt_init.py
+++ command/mqtt_init.py
@@ -1,7 +1,14 @@
 import paho.mqtt.client as mqtt
 
-client = mqtt.Client(client_id="1", clean_session=True, userdata=None, protocol=mqtt.MQTTv311, transport="tcp")
+client = mqtt.Client(client_id="Command Dashboard", clean_session=True, userdata=None, protocol=mqtt.MQTTv311, transport="tcp")
 
-client.connect("192.168.57.26", port=1883, keepalive=60, bind_address="")
+client.connect("192.168.123.26", port=1883, keepalive=60, bind_address="")
 
-client.reconnect_delay_set(5)
+def custom_reconnect(client: mqtt.Client, *args, **kwargs) :
+    print("disconnected")
+    client.reconnect()
+    print("reconnected")
+
+# client.on_disconnect = custom_reconnect
+
+client.loop_start()

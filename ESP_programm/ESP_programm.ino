6a3029e394fbef5bebd924fd1dff962e9ea377b8
Adriaan <adriaanboxmail@gmail.com>
2023-03-27 20:12:54 +0200

    updated README.md for setup

--- ESP_programm/ESP_programm.ino
+++ ESP_programm/ESP_programm.ino
@@ -9,8 +9,8 @@ int nb_leds = 4;
 // PARTIE CONNEXION =====================================================================================================
 
 // Update these with values suitable for your network.
-const char* ssid = "Chiaomi";
-const char* password = "akinoMD4C";
+const char* ssid = "";
+const char* password = "";
 
 const char* mqtt_server = "192.168.123.26";
 const int mqtt_port = 1883;
